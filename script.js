66f0b03d1f68dd2af43aa68f5c531da57699700f
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2021-12-05 16:19:55 -0800

    validation for email

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordCheck = document.getElementById("password-check");
 
 username.addEventListener("focus", () => {
   username.parentElement.classList.remove("success", "error");
+});
+
+email.addEventListener("focus", () => {
+  email.parentElement.classList.remove("success", "error");
+});
+
+password.addEventListener("focus", () => {
+  password.parentElement.classList.remove("success", "error");
+});
+
+passwordCheck.addEventListener("focus", () => {
+  passwordCheck.parentElement.classList.remove("success", "error");
 });
 
 username.addEventListener("blur", (e) => {
@@ -11,3 +26,27 @@ username.addEventListener("blur", (e) => {
     username.parentElement.classList.add("error");
   }
 });
+
+email.addEventListener("blur", (e) => {
+  if (email.validity.valid) {
+    email.parentElement.classList.add("success");
+  } else if (email.value.length > 0) {
+    email.parentElement.classList.add("error");
+  }
+});
+
+password.addEventListener("blur", (e) => {
+  if (password.validity.valid) {
+    password.parentElement.classList.add("success");
+  } else if (password.value.length > 0) {
+    password.parentElement.classList.add("error");
+  }
+});
+
+// email.addEventListener("blur", (e) => {
+//   if (email.validity.valid) {
+//     email.parentElement.classList.add("success");
+//   } else if (email.value.length > 0) {
+//     email.parentElement.classList.add("error");
+//   }
+// });
