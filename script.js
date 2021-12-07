e73d46af2f45252634d9e201389804357951a58f
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2021-12-06 16:38:20 -0800

    javascript refactor

--- script.js
+++ script.js
@@ -1,52 +1,30 @@
+const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const passwordCheck = document.getElementById("password-check");
 
-username.addEventListener("focus", () => {
-  username.parentElement.classList.remove("success", "error");
-});
+const formElements = [username, email, password, passwordCheck];
 
-email.addEventListener("focus", () => {
-  email.parentElement.classList.remove("success", "error");
-});
+formElements.forEach((formElement) => {
+  formElement.addEventListener("focus", () => {
+    formElement.parentElement.classList.remove("success", "error");
+  });
 
-password.addEventListener("focus", () => {
-  password.parentElement.classList.remove("success", "error");
-});
-
-passwordCheck.addEventListener("focus", () => {
-  passwordCheck.parentElement.classList.remove("success", "error");
-});
-
-username.addEventListener("blur", (e) => {
-  if (username.validity.valid) {
-    username.parentElement.classList.add("success");
-  } else if (username.value.length > 0) {
-    username.parentElement.classList.add("error");
-  }
-});
-
-email.addEventListener("blur", (e) => {
-  if (email.validity.valid) {
-    email.parentElement.classList.add("success");
-  } else if (email.value.length > 0) {
-    email.parentElement.classList.add("error");
-  }
-});
-
-password.addEventListener("blur", (e) => {
-  if (password.validity.valid) {
-    password.parentElement.classList.add("success");
-  } else if (password.value.length > 0) {
-    password.parentElement.classList.add("error");
-  }
-});
-
-passwordCheck.addEventListener("blur", (e) => {
-  if (password.validity.valid && passwordCheck.value == password.value) {
-    passwordCheck.parentElement.classList.add("success");
-  } else if (passwordCheck.value.length > 0) {
-    passwordCheck.parentElement.classList.add("error");
-  }
+  formElement.addEventListener("blur", (event) => {
+    if (formElement.id === "password-check") {
+      if (password.validity.valid && passwordCheck.value == password.value) {
+        passwordCheck.parentElement.classList.add("success");
+      } else if (formElement.value.length > 0) {
+        formElement.parentElement.classList.add("error");
+      }
+    } else {
+      if (formElement.validity.valid) {
+        formElement.parentElement.classList.add("success");
+      } else if (formElement.value.length > 0) {
+        formElement.parentElement.classList.add("error");
+        event.preventDefault;
+      }
+    }
+  });
 });
